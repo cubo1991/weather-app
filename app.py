9d7e0314370f851d35cc15a923b45dddeb75433e
Sabrina Moreira <sabrinaarando2002@gmail.com>
2024-10-17 23:11:38 -0300

    manejo de errores

--- app.py
+++ app.py
@@ -6,20 +6,30 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 
 def obtener_clima(ciudad):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric'
-    respuesta = requests.get(url)
-    if respuesta.status_code == 200:
-        datos = respuesta.json()
-        clima = datos['main']
-        print(f"Ciudad: {ciudad}")
-        print(f"Temperatura: {clima['temp']}°C")
-        print(f"Maxima: {clima['temp_max']}°C")
-        print(f"Minima: {clima['temp_min']}°C")
-        print(f"Humedad: {clima['humidity']}%")
-    else:
-        print(f"Error: La ciudad '{ciudad}' no fue encontrada.")
-     
+   
+    try :
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric'
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            datos = respuesta.json()
+            clima = datos['main']
+            print(f"Ciudad: {ciudad}")
+            print(f"Temperatura: {clima['temp']}°C")
+            print(f"Maxima: {clima['temp_max']}°C")
+            print(f"Minima: {clima['temp_min']}°C")
+            print(f"Humedad: {clima['humidity']}%")
+        elif respuesta.status_code == 404:
+                # Si la ciudad no existe
+                print(f"Error: La ciudad '{ciudad}' no fue encontrada.")
+        else:
+            # Otros errores relacionados con la API
+            print(f"Error: No se pudo obtener el clima para '{ciudad}'. (Código {respuesta.status_code})")
+    except requests.exceptions.RequestException as e:
+        # Manejo de cualquier otro error de conexión o solicitud
+        print("Error: Ocurrió un problema al intentar conectarse a la API. Inténtalo de nuevo más tarde.")
+        print(f"Detalles técnicos: {e}")
 
-if __name__ == '__main__':
+
+if __.name__ == '__main__':
     ciudad = input("Ingrese el nombre de la ciudad: ")
     obtener_clima(ciudad)
