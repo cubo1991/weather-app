6666a6d36c25b6b776d9bb0638cad9b2d8ceac38
cubo1991 <adavidlopezmathez@gmail.com>
2024-10-17 21:04:20 -0300

    Lógica para obtener y mostrar los datos del clima

--- app.py
+++ app.py
@@ -6,9 +6,20 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 
 def obtener_clima(ciudad):
-    print("Estas buscando datos de la ciudad:", ciudad)
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric'
+    respuesta = requests.get(url)
+    if respuesta.status_code == 200:
+        datos = respuesta.json()
+        clima = datos['main']
+        print(f"Ciudad: {ciudad}")
+        print(f"Temperatura: {clima['temp']}°C")
+        print(f"Maxima: {clima['temp_max']}°C")
+        print(f"Minima: {clima['temp_min']}°C")
+        print(f"Humedad: {clima['humidity']}%")
+    else:
+        print(f"Error: La ciudad '{ciudad}' no fue encontrada.")
+     
 
 if __name__ == '__main__':
     ciudad = input("Ingrese el nombre de la ciudad: ")
     obtener_clima(ciudad)
-    
