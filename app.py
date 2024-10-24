bb34b84f6c0e39ac5410cbfe4a4129c95ecf79a8
tomas ferro <tomasferro.dev@gmail.com>
2024-10-24 18:30:14 -0300

    feature input for choosing Tº unity

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from llamadaApi import llamadaApi  # Importar la función desde llamadaApi.py
-
-def obtener_clima(ciudad):
+from cambiarTemperatura import cambiarTemperatura 
+def obtener_clima(ciudad, unidad):
     while True:
         datos = llamadaApi(ciudad)  # Llamar a la función para obtener los datos
         
         if datos is not None:  # Si hay datos, se extrae el clima
             clima = datos['main']
             print(f"Ciudad: {ciudad}")
-            print(f"Temperatura: {clima['temp']}°C")
-            print(f"Maxima: {clima['temp_max']}°C")
-            print(f"Minima: {clima['temp_min']}°C")
+            print(f"Temperatura: {clima['temp']}, {unidad}")
+            print(f"Maxima: {clima['temp_max']}, {unidad}")
+            print(f"Minima: {clima['temp_min']}, {unidad}")
             print(f"Humedad: {clima['humidity']}%")
         else:
             print(f"No se pudo obtener el clima para '{ciudad}' debido a un error.")
@@ -18,11 +18,16 @@ def obtener_clima(ciudad):
         if continuar.upper() == "NO":
             break 
         else: 
+            
             ciudad = input("Ingrese el nombre de la ciudad: ")
-          
+            unidad = input("Ingrese la unidad de Temperatura deseada: ")
+            cambiarTemperatura(unidad)
+
          
 
             
 if __name__ == '__main__':
     ciudad = input("Ingrese el nombre de la ciudad: ")
-    obtener_clima(ciudad)
+    unidad = input("Ingrese la unidad de Temperatura deseada: ")
+    cambiarTemperatura(unidad)
+    obtener_clima(ciudad, unidad)
