aa25bb362b763b28315424488fc24cbd14c0273b
cubo1991 <adavidlopezmathez@gmail.com>
2024-10-30 18:01:02 -0300

    algunas cuestiones arregladas

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from llamadaApi import llamadaApi  # Importar la función desde llamadaApi.py
-
+from obtenerClima import obtener_clima
 from pronosticoExtendido import pronostico_extendido
 
 from cambiarTemperatura import cambiarTemperatura 
-def obtener_clima(ciudad, unidad):
-    simbolo, unidad = cambiarTemperatura(unidad)
+def main():
 
 
+    def mostrar_menu():
+        print("----- Menú -----")
+        print("1. Obtener clima")
+        print("2. Pronóstico extendido")
+        print("3. Salir")
+        print("----------------")
+    
     while True:
         mostrar_menu()
         opcion = input("Seleccione una opción (1-3): ")
@@ -30,9 +36,7 @@ def obtener_clima(ciudad, unidad):
             print("Opción no válida. Por favor, intente de nuevo.")
 
 if __name__ == '__main__':
-    ciudad = input("Ingrese el nombre de la ciudad: ")
-    unidad = input("Ingrese unidad de temperatura: ")
-    #obtener_clima(ciudad)
-    pronostico_extendido(ciudad, unidad)
-    
+
+    main()
+
 
